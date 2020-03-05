5c6238c2fa8045d411bb3affa6a7d732b50c88ce
Heisenberg017 <suyash9211@gmail.com>
2020-03-05 12:59:26 +0530

    Design Update : Designing Inner Page (Services Page Completed)

--- dist/script/custom.js
+++ dist/script/custom.js
@@ -35,3 +35,99 @@ $(document).on('click', '.js-menu_toggle.opened', function (e) {
     });
     $('.list_load').slideUp(300);
 });
+
+
+//Toggle Divs
+$(document).ready(function () {
+
+    $(".content-one-link").click(function () {
+        $("#list-one").fadeIn("slow");
+        $(".content-one").hide();
+        $("#list-two").hide();
+        $(".content-two").show("slow");
+        $("#list-three").hide();
+        $(".content-three").show("slow");
+        $("#list-four").hide();
+        $(".content-four").show("slow");
+        $("#list-five").hide();
+        $(".content-five").show("slow");
+        $("#list-six").hide();
+        $(".content-six").show("slow");
+    });
+
+    $(".content-two-link").click(function () {
+        $("#list-one").hide();
+        $(".content-one").show("slow");
+        $("#list-two").fadeIn("slow");
+        $(".content-two").hide();
+        $("#list-three").hide();
+        $(".content-three").show("slow");
+        $("#list-four").hide();
+        $(".content-four").show("slow");
+        $("#list-five").hide();
+        $(".content-five").show("slow");
+        $("#list-six").hide();
+        $(".content-six").show("slow");
+    });
+
+    $(".content-three-link").click(function () {
+        $("#list-one").hide();
+        $(".content-one").show("slow");
+        $("#list-two").hide();
+        $(".content-two").show("slow");
+        $("#list-three").fadeIn("slow");
+        $(".content-three").hide();
+        $("#list-four").hide();
+        $(".content-four").show("slow");
+        $("#list-five").hide();
+        $(".content-five").show("slow");
+        $("#list-six").hide();
+        $(".content-six").show("slow");
+    });
+
+    $(".content-four-link").click(function () {
+        $("#list-one").hide();
+        $(".content-one").show("slow");
+        $("#list-two").hide();
+        $(".content-two").show("slow");
+        $("#list-three").hide();
+        $(".content-three").show("slow");
+        $("#list-four").fadeIn("slow");
+        $(".content-four").hide();
+        $("#list-five").hide();
+        $(".content-five").show("slow");
+        $("#list-six").hide();
+        $(".content-six").show("slow");
+    });
+
+    $(".content-five-link").click(function () {
+        $("#list-one").hide();
+        $(".content-one").show("slow");
+        $("#list-two").hide();
+        $(".content-two").show("slow");
+        $("#list-three").hide();
+        $(".content-three").show("slow");
+        $("#list-four").hide();
+        $(".content-four").show("slow");
+        $("#list-five").fadeIn("slow");
+        $(".content-five").hide();
+        $("#list-six").hide();
+        $(".content-six").show("slow");
+    });
+
+    $(".content-six-link").click(function () {
+        $("#list-one").hide();
+        $(".content-one").show("slow");
+        $("#list-two").hide();
+        $(".content-two").show("slow");
+        $("#list-three").hide();
+        $(".content-three").show("slow");
+        $("#list-four").hide();
+        $(".content-four").show("slow");
+        $("#list-five").hide();
+        $(".content-five").show("slow");
+        $("#list-six").fadeIn("slow");
+        $(".content-six").hide();
+    });
+
+});
