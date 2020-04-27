c8c32b6ce73f5a1ac328dc3431aaca4b04d91ab9
Heisenberg017 <suyash9211@gmail.com>
2020-04-27 23:39:23 +0530

    Dashboard Completed

--- dist/portfolios(back).php
+++ dist/portfolios(back).php
@@ -1,4 +1,8 @@
-<?php include('../dashboard/includes/connectionClass.php'); ?>
+<?php include_once("../dashboard-static/includes/connectionClass.php");
+    $display_query="SELECT * FROM krackpottb_demo_1 ORDER BY id DESC";
+    $run_query=mysqli_query($connect,$display_query);
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -16,107 +20,85 @@
 
 <body>
     <div class="section portfolio-section-list">
-        <nav class="navbar navbar-portfolio-list">
+        <nav class="navbar navbar-portfolio-list fixed-top">
             <?php include('includes/sidebar.php') ?>
+
         </nav>
-
-
         <div class="container-fluid">
             <div id="section-carousel" class="carousel slide" data-ride="carousel" data-interval="false">
                 <div class="carousel-inner">
-                <?php
-                
-                    $connect = mysqli_connect("localhost","root","","krackpot");
+                    <?php if($run_query->num_rows>0){
+                                    while($row=$run_query->fetch_assoc()){
+                            ?>
+                    <div class="carousel-item">
+                        <div class="row section-info">
+                            
+                            <div class="offset-1 col-lg-5 col-xl-5 col-md-12 col-sm-12 col-xs-12 section-info-main">
+                                <div class="portfolio-section-list-main animated fadeInLeft">
+                                
+                                    <img src="../dashboard-static/uploads/<?php echo $row['brand_logo'];?>" class="clients-img">
+                                    <p class="clients-description"><?php echo $row['brand_description'];?></p>
 
-                    function make_query($connect){
-                        $display_query ="SELECT * FROM krackpottb ORDER BY id DESC" ;
-                        $display_query_result = mysqli_query($connect,$display_query);
-                        return $display_query_result;
-                    }
+                                    <h1 class="work-text -title"><?php echo $row['brand_work_header'];?></h1>
+                                    <h1 class="work-text -subtitle"><?php echo $row['brand_work_description'];?>
+                                    </h1>
+                                </div>
+                            </div>
+                            <div class="col-xl-5 col-lg-5 col-md-12 col-sm-12 col-xs-12 section-info-main col-gap">
+                                <div class="section-info-start animated fadeInRight">
+                                    <div id="clients-slider" class="clients-slider">
+                                            <!-- <img src="images/clients/0fila1.png" alt="Clients Brand Image"
+                                                class="clients-slider-img">
+                                                <img src="images/clients/1fila2.png" alt="Clients Brand Image"
+                                                class="clients-slider-img">
+                                                 <img src="images/clients/2fila3.png" alt="Clients Brand Image"
+                                                class="clients-slider-img">
+                                                 <iframe class="clients-slider-videos"
+                                                src="https://www.youtube.com/embed/uE1m21Umn0s" frameborder="0"
+                                                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                                                allowfullscreen>
+                                            </iframe> -->
 
-                    /* function files_query($connect){
-                        $display_files_query ="SELECT * FROM krackpotbrandimages" ;
-                        $display_files_query_result = mysqli_query($connect,$display_files_query);
-                        return $display_files_query_result;
-                    } */
+                                            <?php 
 
-                    function make_slides($connect){
-                        $output = '';
-                        $count = 0;
-                        $result = make_query($connect);
-                            while($row = mysqli_fetch_assoc($result)){
-                                if($count == 0){
-                                    $output .= '<div class="carousel-item active">
-                                    <div class="row section-info">
-                                    <div class="offset-1 col-lg-5 col-xl-5 col-md-12 col-sm-12 col-xs-12 section-info-main">
-                                    <div class="portfolio-section-list-main animated fadeInLeft">';
-                                }
-                                else{
-                                    $output .='</div></div></div></div>
-                                            <div class="carousel-item"> 
-                                            <div class="row section-info">
-                                            <div class="offset-1 col-lg-5 col-xl-5 col-md-12 col-sm-12 col-xs-12 section-info-main">
-                                            <div class="portfolio-section-list-main animated fadeInLeft">
-                                            ';
-                                } 
-                                $output .='<img src="../dashboard/uploads/'.$row['brand_logo'].'" class="clients-img">
-                                    <p class="clients-description">'.$row['brand_description'].'</p>
-                                    <h1 class="work-text -title">'.$row['brand_work_header'].'</h1>
-                                    <h1 class="work-text -subtitle">'.$row['brand_work_description'].'</h1>';
-                                    
-                                    $count = $count + 1;
-                            }
-                        return $output;
-                        }
-                ?>
-                        <?php echo make_slides($connect); ?>
-
-                        <?php 
-
-                            function make_files_query($connect){
-                                $output = '';
-                                $count = 0;
-                                $result = files_query($connect);
-                                    while($row = mysqli_fetch_assoc($result)){
-                                        if($count == 0){
-                                            $output .= '<div id="clients-slider" class="clients-slider">';
-                                        }
-                                        else{
-                                            $output .='</div></div></div>
-                                                    ';
-                                        } 
-                                        $output .='<img src="../dashboard/uploads/'.$row['Image'].'" class="clients-slider-img">';
-                                            
-                                            $count = $count + 1;
-                                    }
-                                return $output;
-                                }
-
-                        ?>
-                        
+                                                $i="";
+                                                $query="SELECT * FROM krackpottb_demo_1 a, attachments b WHERE a.id=b.attachment_id AND a.id = ".$row['id'];
+                                                $fire=mysqli_query($connect,$query);
+                                                $data=mysqli_fetch_assoc($fire);
+                                                $res=$data['images'];
+                                                $res=explode(" ",$res);
+                                                $count=count($res)-1;
+                                                for($i=0;$i<$count;++$i){
+                                                ?>
+                                                <img src="../dashboard-static/uploads/<?php echo $res[$i] ?>" class="clients-slider-img"/>
+                                                
+                                                <?php } ?>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                </div>
-                </div>
+                     <?php } } ?>
                 </div>
                 <div class="carousel-actions">
                     <div class="row">
                         <div class="offset-1 col-xl-10 col-lg-10 col-md-10 col-sm-12 col-xs-12">
                             <a class="carousel-control-next clients-carousel-next" href="#section-carousel"
-                        role="button" data-slide="next">
-                        <span class="next-text">Next</span>
-                        <span class="carousel-control-next-icon clients-carousel-next-icon "
-                            aria-hidden="true"><span class="next-text"></span></span>
-                        <span class="carousel-control-next-icon clients-carousel-next-icon "
-                            aria-hidden="true"></span>
-                        <a class="carousel-control-prev clients-carousel-prev" href="#section-carousel"
-                        role="button" data-slide="prev">
-                        <span class="carousel-control-prev-icon clients-carousel-prev-icon "
-                            aria-hidden="true"><span class="next-text"></span></span>
-                        <span class="carousel-control-prev-icon clients-carousel-prev-icon "
-                            aria-hidden="true"></span>
-                        <span class="next-text">Previous</span>
-                    </a>
+                                role="button" data-slide="next">
+                                <span class="next-text">Next</span>
+                                <span class="carousel-control-next-icon clients-carousel-next-icon "
+                                    aria-hidden="true"><span class="next-text"></span></span>
+                                <span class="carousel-control-next-icon clients-carousel-next-icon "
+                                    aria-hidden="true"></span>
+                            </a>
+                            <a class="carousel-control-prev clients-carousel-prev" href="#section-carousel"
+                                role="button" data-slide="prev">
+                                <span class="carousel-control-prev-icon clients-carousel-prev-icon "
+                                    aria-hidden="true"><span class="next-text"></span></span>
+                                <span class="carousel-control-prev-icon clients-carousel-prev-icon "
+                                    aria-hidden="true"></span>
+                                <span class="next-text">Previous</span>
+                            </a>
                             <div class="back-to-portfolio">
                                 <a href="portfolio.php" class="back-to-portfolio-btn">back to portfolio</a>
                             </div>
@@ -142,8 +124,18 @@
                 nextArrow: '<button class="slide-arrow next-arrow fas fa-chevron-right"></button>',
             })
         });
-    </script>
 
+        $('.carousel').carousel({
+            interval:false
+        })
+        $(".carousel-inner .carousel-item:first-child" ).addClass('active');
+        
+        if($('a.carousel-item:first-child').hasClass('active'))
+        {
+            $('a.carousel-control-next').css('display','none');
+            
+        }
+    </script>
 </body>
 
 </html>
